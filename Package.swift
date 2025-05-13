8268c9f0f2679d2cee4084ddb4b251807321a407
Dmytro Shumakov <shumakov_d@hotmail.com>
2025-05-13 16:51:18 +0300

    Update

--- Package.swift
+++ Package.swift
@@ -38,11 +38,11 @@ let package = Package(
             targets: ["IronSourceTarget"]
         ),
     ],
-        
+    
     dependencies: [
-         .package(url: "https://github.com/AppLovin/AppLovin-MAX-Swift-Package", from: "13.2.0"),
-         .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads", from: "12.3.0"),
-         .package(url: "https://github.com/Vungle/VungleAdsSDK-SwiftPackageManager", from: "7.4.5"),
+        .package(url: "https://github.com/AppLovin/AppLovin-MAX-Swift-Package", from: "13.2.0"),
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads", from: "12.3.0"),
+        .package(url: "https://github.com/Vungle/VungleAdsSDK-SwiftPackageManager", from: "7.4.5"),
         .package(url: "https://github.com/Mintegral-official/MintegralAdSDK-Swift-Package", from: "7.7.7")
     ],
     
